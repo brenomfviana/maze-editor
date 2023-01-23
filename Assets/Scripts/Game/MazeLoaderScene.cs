1de253cb7d94922bc873a91fb3bec307eaa82978
Breno Viana <brenomfviana@gmail.com>
2023-01-23 11:21:23 -0300

    implements animation of the generation

--- Assets/Scripts/Game/MazeLoaderScene.cs
+++ Assets/Scripts/Game/MazeLoaderScene.cs
@@ -11,14 +11,73 @@ public class MazeLoaderScene : MonoBehaviour
     public Tilemap tilemap;
     public GameObject cell;
 
+    private Generator g;
+    private Maze maze;
+    private bool next;
+
     void Awake()
     {
+        // LoadCurrentLevel();
+        g = new Generator(Random.Range(0, 1000), 6, 6);
+        next = true;
+        maze = g.maze;
+        Centralize();
+    }
+
+    void Update()
+    {
+        if (next)
+        {
+            next = false;
+            StartCoroutine(GenerateLevel());
+        }
+    }
+
+    IEnumerator GenerateLevel()
+    {
+        maze = g.GenerateNext();
         LoadCurrentLevel();
+        yield return new WaitForSeconds(0.5f);
+        next = true;
+    }
+
+    private void Centralize()
+    {
+        Vector3Int crtCell = tilemap.WorldToCell(transform.position);
+        int ix = crtCell.x;
+        Vector3? center = new Vector3(0, 0, 0);
+        for (uint i = 0; i < maze.Height; i++)
+        {
+            for (uint j = 0; j < maze.Width; j++)
+            {
+                Vector3 spawnPoint = tilemap.GetCellCenterWorld(tilemap.WorldToCell(transform.position)) + crtCell;
+                GameObject tile = Instantiate(cell, spawnPoint, Quaternion.identity, grid.transform);
+                SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+                sr.sprite = GetSprite(maze, i, j);
+                Vector3? aux = GetMazeCenter(maze, i, j, spawnPoint);
+                center = aux != null ? FixMazeCenter((Vector3)aux, i, j) : center;
+                crtCell.x += 1;
+            }
+            crtCell.x = ix;
+            crtCell.y -= 1;
+        }
+        mainCamera.Move((Vector3)center);
+    }
+
+    private void LoadNext()
+    {
+        // GameObject tile = Instantiate(cell, spawnPoint, Quaternion.identity, grid.transform);
+        // TearDownWalls(tile, maze, i, j);
     }
 
     private void LoadCurrentLevel()
     {
-        Maze maze = Generator.Generate(Random.Range(0, 1000), 6, 6);
+         foreach (Transform child in grid.transform) {
+            if (child != tilemap.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
         Vector3Int crtCell = tilemap.WorldToCell(transform.position);
         int ix = crtCell.x;
         Vector3? center = new Vector3(0, 0, 0);
--- Assets/Scripts/Generator/Generator.cs
+++ Assets/Scripts/Generator/Generator.cs
@@ -2,36 +2,71 @@ using UnityEngine;
 
 class Generator
 {
-    public static Maze Generate(int seed, uint width, uint height)
+    private int seed;
+    public Maze maze;
+    private uint next;
+    private uint _in;
+    private uint _jn;
+
+    public Generator(int seed, uint width, uint height)
     {
         Random.InitState(seed);
-        Maze maze = new Maze(width, height);
+        maze = new Maze(width, height);
+        next = 0;
+        _in = 0;
+        _jn = 0;
+    }
+
+    public Maze Generate()
+    {
         for (uint i = 0; i < maze.Height; i++)
         {
             for (uint j = 0; j < maze.Width; j++)
             {
-                if (i == maze.Height - 1)
-                {
-                    maze.TearDownEastWall(i, j);
-                }
-                else if (j == maze.Width - 1)
-                {
-                    maze.TearDownSouthWall(i, j);
-                }
-                else
-                {
-                    bool r = Random.Range(0, 2) == 0;
-                    if (r && maze.IsEastWallUp(i, j))
-                    {
-                        maze.TearDownEastWall(i, j);
-                    }
-                    else if (i < maze.Height - 1)
-                    {
-                        maze.TearDownSouthWall(i, j);
-                    }
-                }
+                GenerateIterative(i, j);
             }
         }
         return maze;
+    }
+
+    public Maze GenerateNext()
+    {
+        Debug.Log(_in + " " + _jn);
+        if (_jn < maze.Width && _in < maze.Height) {
+            GenerateIterative(_in, _jn);
+        }
+        if (_jn < maze.Width)
+        {
+            _jn++;
+        }
+        if (_jn == maze.Width && _in < maze.Height) {
+            _jn = 0;
+            _in++;
+        }
+        return maze;
+    }
+
+    private void GenerateIterative(uint i, uint j)
+    {
+        if (i == maze.Height - 1)
+        {
+            maze.TearDownEastWall(i, j);
+        }
+        else if (j == maze.Width - 1)
+        {
+            maze.TearDownSouthWall(i, j);
+        }
+        else
+        {
+            bool r = Random.Range(0, 2) == 0;
+            if (r && maze.IsEastWallUp(i, j))
+            {
+                maze.TearDownEastWall(i, j);
+            }
+            else if (i < maze.Height - 1)
+            {
+                maze.TearDownSouthWall(i, j);
+            }
+        }
     }
 }
